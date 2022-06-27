5412f17866343ed67980e1ca353bc44257778f0a
qiruwa <C:\Users\anohin\AppData\Roaming\The Bat!>
2022-06-27 15:20:38 +0300

    1

--- string_sum.go
+++ string_sum.go
@@ -2,10 +2,15 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
+	n             = 1
+	op1, op2, sum int
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
@@ -23,5 +28,43 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	input = strings.ReplaceAll(input, " ", "")
+	if input == "" {
+		return "", fmt.Errorf("Gimme numbers!, %w", errorEmptyInput)
+	}
+
+	if strings.HasPrefix(input, "-") {
+		n *= -1
+		input = strings.TrimLeft(input, "-")
+	}
+
+	if strings.Count(input, "+")+strings.Count(input, "-") != 1 {
+		return "", fmt.Errorf("%w", errorNotTwoOperands)
+	}
+	m := make([]string, 2)
+	if strings.Contains(input, "+") {
+		m = strings.Split(input, "+")
+		op1, err = strconv.Atoi(m[0])
+		if err != nil {
+			return "", fmt.Errorf("%w", err)
+		}
+		op2, err = strconv.Atoi(m[1])
+		if err != nil {
+			return "", fmt.Errorf("%w", err)
+		}
+		sum = n*op1 + op2
+
+	} else {
+		m = strings.Split(input, "-")
+		op1, err = strconv.Atoi(m[0])
+		if err != nil {
+			return "", fmt.Errorf("%w", err)
+		}
+		op2, err = strconv.Atoi(m[1])
+		if err != nil {
+			return "", fmt.Errorf("%w", err)
+		}
+		sum = n*op1 - op2
+	}
+	return strconv.Itoa(sum), nil
 }
